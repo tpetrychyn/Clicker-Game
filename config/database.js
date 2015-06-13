453df6a61b5720934aa91f5c9b68f0bdc5743b66
Taylor Petrychyn <tpetrychyn@gmail.com>
2015-06-13 02:08:30 -0600

    fixed database auth

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://heroku_4lhh9rtj:heroku_4lhh9rtj@ds047642.mongolab.com:47642/heroku_4lhh9rtj' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //'url' : 'mongodb://a:a@apollo.modulusmongo.net:27017/ym2axIhi' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_admin:Marmoset93$$@ds047642.mongolab.com:47642/heroku_4lhh9rtj'
 
 };
